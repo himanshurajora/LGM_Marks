01a2614905f57d5b6a7f351f409aef94aa893376
Himanshu Jangid <iamahalofan@gmail.com>
2021-09-16 19:10:12 +0530

    get stududents by class

--- Database/Students.php
+++ Database/Students.php
@@ -11,8 +11,16 @@
             $statement->execute();
             return $statement->fetchAll(PDO::FETCH_ASSOC);
         }
+        public function getStudentByClassno($classno){
+            $query = "SELECT * FROM students WHERE classno = :classno";
+            $statement = $this->connect->prepare($query);
+            $statement->bindParam(':classno', $classno, PDO::PARAM_INT);
+            $statement->execute();
+            return $statement->fetchAll(PDO::FETCH_ASSOC);
+        }
         public function addStudent($name, $classno, $rollno, $age, $gender){
             $query = "INSERT INTO students (name, classno, rollno, age, gender) VALUES (:name, :classno, :rollno, :age, :gender)";
+            $query2 = "INSERT INTO results (studentid, result) VALUES ($rollno, '')";
             $statement = $this->connect->prepare($query);
             $statement->bindParam(':name', $name, PDO::PARAM_STR);
             $statement->bindParam(':classno', $classno, PDO::PARAM_INT);
@@ -20,6 +28,10 @@
             $statement->bindParam(':age', $age, PDO::PARAM_INT);
             $statement->bindParam(':gender', $gender, PDO::PARAM_STR);
             $statement->execute();
+
+            $statement = $this->connect->prepare($query2);
+            $statement->execute();
+            
             return "Added Student $name in class $classno Successfully";
         }
         public function deleteStudent($id){
